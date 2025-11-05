b264c1a4b5b2762102345b9b8955284ad39e5bed
severo <oliveira.severo@gmail.com>
2025-11-05 14:29:47 -0300

    feat: :lipstick: Cria CSS Resset

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,11 @@
 import { useState } from 'react';
 // import viteLogo from 'vite.svg';
-import reactLogo from './assets/react.svg';
-import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button type="button" onClick={() => setCount((count) => count + 1)}>
